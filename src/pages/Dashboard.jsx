23c090db9eb1bb46a24bc0c7e69d3232d9975436
unknown <kiranindia1986@gmail.com>
2024-11-27 18:23:27 +0530

    pushing

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -101,6 +101,7 @@ const RightSideContainer = styled.div`
 
 const TableWrapper = styled.div`
   height: 400px; /* Set a fixed height for the table */
+  max-width: 1400px; /* Set a fixed width for the table */
   overflow: auto; /* Enable both horizontal and vertical scrolling */
   border: 1px solid #ddd; /* Optional border around the table */
   margin-top: 20px;
