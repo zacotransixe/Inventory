a356e3776daf3a0e36f6284014ab263f51a380d2
Kiran <kiranindia1986@gmail.com>
2024-12-18 17:46:45 +0530

    new database

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth'; // Import Firebase Auth
 
 // Your Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDrsT6GIcLyOI1xyQ0RxG3p7VewJGyneL4",
-    authDomain: "inventory-26dd2.firebaseapp.com",
-    projectId: "inventory-26dd2",
-    storageBucket: "inventory-26dd2.appspot.com",
-    messagingSenderId: "609100816128",
-    appId: "1:609100816128:web:315282db84bdf5cc87dde5"
+    apiKey: "AIzaSyCnknVsQrKnWpuF6S48OnonihG3NUM-4z0",
+    authDomain: "inventory-763d9.firebaseapp.com",
+    projectId: "inventory-763d9",
+    storageBucket: "inventory-763d9.firebasestorage.app",
+    messagingSenderId: "115625522653",
+    appId: "1:115625522653:web:c08ba532037a347f89f234"
 };
 
 // Initialize Firebase  
